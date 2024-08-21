2e028724ad41cc3f203094ac53ed7a66a321c9e7
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-08-21 06:44:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setuptools.setup(
         )
     ],
     cmdclass={"build_ext": cmake_build_extension.BuildExtension},
-    package_data={
-        "": [r"C:\windows\system32\vcomp140.dll"],  # Include the DLL in the package
-    }
+    package_data={'': [r"C:\windows\system32\vcomp140.dll"]},
+    include_package_data=True,
+    
 )
